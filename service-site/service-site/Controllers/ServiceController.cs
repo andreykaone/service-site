a1206e711d339c361dc380c3896cb9a8901c6a15
kormilitsyn.andrey <andreyka.one.two@gmail.com>
2019-04-03 01:10:40 +0300

    Initial commit

---
+++ service-site/service-site/Controllers/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using service_site.Models;
+
+namespace service_site.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ service-site/service-site/Controllers/SiteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using service_site.Models;
+
+namespace service_site.Controllers
+{
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
